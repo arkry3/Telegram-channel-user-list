06d880a0ee52a15c83f7965c73057377eaf03246
arkry3 <nubwer2k19@gmail.com>
2024-11-11 22:44:19 +0100

    Update tg_user_list.py

--- tg_user_list.py
+++ tg_user_list.py
@@ -9,8 +9,8 @@ load_dotenv(dotenv_path='confidential.env')
 api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
 bot_token = os.getenv('BOT_TOKEN')
-chat_link = os.getenv('CHAT_LINK')
-file_path = os.getenv('FILE_PATH')
+group_link = os.getenv('GROUP_LINK')
+table_path = os.getenv('TABLE_PATH')
 channel_link = os.getenv('CHANNEL_LINK')
 
 
@@ -18,24 +18,25 @@ bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 user_data = {'Name': []}
 
 
-@bot.on(events.NewMessage(pattern=r'\.users', chats=chat_link))
+@bot.on(events.NewMessage(pattern=r'\.users', chats=group_link))
 async def handler(event):
     users = await bot.get_participants(channel_link)
     for user in users:
         if user.username is not None:
             user_data['Name'].append(user.username)
-            await bot.send_message(entity=chat_link, message=user.username)
+            await bot.send_message(entity=group_link, message=user.username)
         else:
-            await bot.send_message(entity=chat_link, message='ОШИБКА')
+            await bot.send_message(entity=group_link, message='ОШИБКА')
             user_data['Name'].append('ОШИБКА, НЕТУ НИКА')
-    empty_table(file_path)
+    empty_table(table_path)
     df = pd.DataFrame(user_data)
-    df.to_excel(file_path, index=False, engine='openpyxl')
+    df.to_excel(table_path, index=False, engine='openpyxl')
     await bot.send_message(
-        entity=chat_link, message='Тот же список в виде Excel таблицы'
+        entity=group_link, message='Тот же список в виде Excel таблицы'
     )
-    await bot.send_file(entity=chat_link, file=file_path)
+    await bot.send_file(entity=group_link, file=table_path)
 
 
 bot.start()
 bot.run_until_disconnected()
+
